92450d00353c4750a0fa411446c8fc11aca73fcd
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-08-04 13:32:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const dotenv = require("dotenv");
+
+//
+const app = express();
+dotenv.config();
+const port = 3007;
+
+//
+app.get("/", (req, res) => {
+  res.send("Hello World12!");
+});
+
+//
+//mongoDB cloud
+let uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.te788iv.mongodb.net/assignment-04-aug-23?retryWrites=true&w=majority`;
+//
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
